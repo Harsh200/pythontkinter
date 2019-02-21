1654d0178c150fc28db5938832bddad35a30c181
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:19:09 +0530

    click change

--- button.py
+++ button.py
@@ -2,7 +2,7 @@ from tkinter import *
 window=Tk()
 def handleClick():
     print("Button click")
-btn=Button(window,bd=20,bg="green",fg="red",text="CLock Me",padx=50,pady=80,command=handleClick)
+btn=Button(window,bd=20,bg="green",fg="red",text="CLick Me",padx=50,pady=80,command=handleClick)
 
 btn.place(x=50,y=50)
 
