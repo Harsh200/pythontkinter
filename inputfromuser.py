44f3d6e04d641d0ace7904d82175048f0a8d7395
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:25:03 +0530

    define trace

--- inputfromuser.py
+++ inputfromuser.py
@@ -1,7 +1,8 @@
 from tkinter import *
 
 window=Tk()
-userwrote=StringVar()
+userwrote = StringVar()
+userwrote.trace("w",)
 e=Entry(window,fg="Black",bd=5,bg="black",textvariable=userwrote)
 e.pack()
 window.mainloop()
