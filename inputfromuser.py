82804974721dff9ac8ff981243b25f277189e475
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:23:58 +0530

    define userwrote

--- inputfromuser.py
+++ inputfromuser.py
@@ -2,6 +2,6 @@ from tkinter import *
 
 window=Tk()
 userwrote=StringVar()
-e=Entry(window,fg="Black",bd=5,bg="black")
+e=Entry(window,fg="Black",bd=5,bg="black",textvariable=userwrote)
 e.pack()
 window.mainloop()
