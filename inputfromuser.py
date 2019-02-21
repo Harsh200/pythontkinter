2001e52fc43d89590631b225d7e12098c3d64e1f
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:26:28 +0530

    make printvalue function

--- inputfromuser.py
+++ inputfromuser.py
@@ -1,8 +1,10 @@
 from tkinter import *
+def printvalue():
+    print*userwrote.get())
 
 window=Tk()
 userwrote = StringVar()
-userwrote.trace("w",)
+userwrote.trace("w",lambda name,index,mode:printvalue)
 e=Entry(window,fg="Black",bd=5,bg="black",textvariable=userwrote)
 e.pack()
 window.mainloop()
