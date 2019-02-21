6ca99ade57fcb49dafca49621b125a6d23bf9492
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:14:51 +0530

    change foreground

--- button.py
+++ button.py
@@ -1,5 +1,5 @@
 from tkinter import *
 window=Tk()
-btn=Button(window,bd=20,bg="green",fg="white",text="CLock Me",padx=50,pady=80)
+btn=Button(window,bd=20,bg="green",fg="red",text="CLock Me",padx=50,pady=80)
 btn.place(x=50,y=50)
 window.mainloop()
