67adb3ea146a33598b1957665d68ee886d68261e
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:08:06 +0530

    labels are made

--- frames.py
+++ frames.py
@@ -1,6 +1,15 @@
 from tkinter import *
 
 window=Tk()
-
+ftop=Frame(window)
+ftop.pack()
+fbot = Frame(window)
+fbot.pack(side=BOTTOM)
+lb1=Label(ftop,text="I am harsh")
+lb2=Label(ftop,text="i work on python")
+lb3=Label(fbot,text="what are you waiting for")
+lb1.pack()
+lb2.pack()
+lb3.pack()
 
 window.mainloop()
