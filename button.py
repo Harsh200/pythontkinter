86b2fa9519a9f4174b72b3d669ec5912886fe12a
Harsh200 <harsh.saxena_ccv17@gla.ac.in>
2019-02-21 22:18:34 +0530

    handleclick function is add

--- button.py
+++ button.py
@@ -1,6 +1,8 @@
 from tkinter import *
 window=Tk()
-btn=Button(window,bd=20,bg="green",fg="red",text="CLock Me",padx=50,pady=80)
+def handleClick():
+    print("Button click")
+btn=Button(window,bd=20,bg="green",fg="red",text="CLock Me",padx=50,pady=80,command=handleClick)
 
 btn.place(x=50,y=50)
 
